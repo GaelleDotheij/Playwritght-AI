f1b3b2953119a990131a3e4992b8f62d213347c8
Gaelle Dotheij <Gaelle.Dotheij@endava.com>
2025-10-07 13:00:39 -0300

    last changes for class3 exercice

--- tests/Class3.spec.js
+++ tests/Class3.spec.js
@@ -6,16 +6,17 @@ test('open https://practicesoftwaretesting.com/ and check that the link to creat
   await page.locator('[data-test="nav-sign-in"]').click();
   await page.waitForTimeout(4000); // pausa de 4 segundos
   expect(page.url()).toContain('login');
-  await page.locator('[data-test="Register your account"]').click();
+  await page.locator('[data-test="register-link"]').click();
   expect(page.url()).toContain('register');
   // Use .not for one of the verifications
   await page.waitForTimeout(3000); // pausa de 3 segundos
-  await page.locator('[data-test="New Account Creation"]').not.toBeVisible();
+  await expect(page.locator('[data-test="register-form-complete"]')).not.toBeVisible();
   // Use .soft to validate multiple fields in the account registration form
-  await page.locator('[data-test="First name"]').soft().toBeVisible();
-  await page.locator('[data-test="Family name"]').soft().toBeVisible(); //should fail
-  await page.locator('[data-test="Street"]').soft().toBeVisible();
-  await page.locator('[data-test="Number"]').soft().toBeVisible(); //should fail
+  await page.waitForTimeout(3000); // pausa de 3 segundos
+  await expect.soft(page.locator('[data-test="first-name"]')).toBeVisible();
+  await expect.soft(page.locator('[data-test="family-name"]')).toBeVisible(); //should fail
+  await expect.soft(page.locator('[data-test="street"]')).toBeVisible();
+  await expect.soft(page.locator('[data-test="number"]')).toBeVisible(); //should fail
   // Verify Registration button on the Registration Page is visible
   expect(page.locator('[data-test="register-submit"]')).toBeVisible();
 });
